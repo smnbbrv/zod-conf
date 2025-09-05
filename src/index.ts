94ce3fb412084dbc9ff748ec71cfac8c13af3dcc
Simon Bobrov <smnbbrv@users.noreply.github.com>
2025-09-05 14:47:01 +0200

    fix: fix export for ZodConfSchema

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import { boolean, number, string, enum as zenum } from 'zod';
-import { define, ZodConfSchema } from './values/define.js';
+import { define } from './values/define.js';
 import { env } from './values/env.js';
 import { object } from './values/object.js';
+
+export { ZodConfSchema } from './values/define.js';
 
 export const zc = {
   // core values
   define,
   env,
   object,
-  ZodConfSchema,
 
   // re-export common zod values for convenience
   string,
